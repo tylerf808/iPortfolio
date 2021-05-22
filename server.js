3f3082d17d9966e7b9685f0e268dfea86ca387d9
Tyler Fabian <tylerf808@gmail.com>
2021-05-22 13:55:12 -0400

    Added functioning post method to the userRoutes file

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
-
-// Import the connection object
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 const app = express();
@@ -8,6 +7,8 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
 
 // Connect to the database before starting the Express.js server
 sequelize.sync().then(() => {
