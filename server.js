04479010e73e8bf4e4ac670c86076945aaa35d24
Carl Benbow <cbenbow@synergisticagility.com>
2021-05-25 19:31:25 -0400

    updated server to use handlebars, updates handlebars structure

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const routes = require('./controllers');
-const sequelize = require('./config/connection');
+const handlebars = require('express-handlebars')
+// const sequelize = require('./config/connection');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+
+
+app.set('view engine','handlebars')
+app.engine('handlebars',handlebars({layoutsDir: __dirname +'/views/layouts',}))
+app.use(express.static('public'))
+
+app.get('/',(req,res) =>{ res.render('homepage', {layout:'main'})})
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,6 +21,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // Connect to the database before starting the Express.js server
-sequelize.sync().then(() => {
+// sequelize.sync().then(() => {
     app.listen(PORT, () => console.log('Now listening'));
-});
+// });
