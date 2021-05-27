cb190f544f56ad21074251e33ae9e565f9195a6d
unknown <jacobchrisb@gmail.com>
2021-05-26 20:53:19 -0400

    style.css, stockcarddetails.handlebars

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const handlebars = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+app.set('view engine','handlebars')
+app.engine('handlebars',handlebars({layoutsDir: __dirname +'/views/layouts',}))
+app.use(express.static('public'))
+app.get('/', (req, res) =>{res.render('homepage', {layout:'main'})
+
+})
+app.get('/watchlist', (req, res) =>{res.render('stockcarddetails', {layout:'main'})
+
+})
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
+//sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
-});
+//});
