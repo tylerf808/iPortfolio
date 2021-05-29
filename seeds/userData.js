ac758d063797494f1c775a6d55108eb4633d7711
Tyler Fabian <tylerfabian@tylers-mbp.lan>
2021-05-29 10:53:48 -0400

    Simplified passwords with no hashing

--- seeds/userData.js
+++ seeds/userData.js
@@ -3,15 +3,15 @@ const { User } = require('../models');
 const userData = [
     {
         email: "email1@gmail.com",
-        password: "$2y$12$U4sL7Q3U0u8hyKwJqVdiguqYsVndVZlmmAdW4pt.M5W3GBGeOz/smxs"
+        password: "password1"
     },
     {
         email: 'email2@gmail.com',
-        password: '$2y$12$z5Q1m2Xht5emwwU/MLiN..UkzVktwBCmNMppo6GH4Uh/4.7VwbNiG'
+        password: 'password2'
     },
     {
         email: 'email3@gmail.com',
-        password: '$2y$12$QSC2JAqoSmTgkj11cXbQSe4WT3iBe2nr39zQyXEUL7H6mwQb573ve '
+        password: 'password3'
     }
 ];
 
