95d3b9ee7796456d460fd79f7bf72517df63fcff
Carl Benbow <cbenbow@synergisticagility.com>
2021-05-29 13:57:40 -0400

    updated the login.js file

--- public/js/login.js
+++ public/js/login.js
@@ -9,28 +9,34 @@ const loginFormHandler = async (event) => {
         method: 'POST',
         body: JSON.stringify({ email, password }),
         headers: { 'Content-Type': 'application/json' },
-      });
-  
-      
-    
+      })
+      if (response.ok) {
+        document.location.replace('/watchlist');
+      } else {
+        alert(response.statusText);
+      }
+
   };
   
   const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    const username = document.querySelector('#username-signup').value.trim();
+    const name = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
   
    
       const response = await fetch('/api/users/create', {
         method: 'POST',
-        body: JSON.stringify({ username, email, password }),
+        body: JSON.stringify({ name, email, password }),
         headers: { 'Content-Type': 'application/json' },
       });
-  
-      
-    
+
+      if (response.ok) {
+        document.location.replace('/watchlist');
+      } else {
+        alert(response.statusText);
+      }
   };
   
   document
