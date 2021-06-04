be536b6fc2cffdd5c54b844ccee8526aa133e455
Tyler Fabian <tylerf808@gmail.com>
2021-06-04 19:41:36 -0400

    Fixed header issue

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,11 +1,7 @@
 module.exports = {
-<<<<<<< HEAD
+
     percentageChange: (currentPrice, purchasePrice) => {
-=======
 
-    percentageChange : (currentPrice, purchasePrice) => {
-
->>>>>>> eb94228a4952054be247db0877520b3c14023d5c
         let difference = currentPrice - purchasePrice;
         let percentage = difference / purchasePrice * 100;
         return percentage;
